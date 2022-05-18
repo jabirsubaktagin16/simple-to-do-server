940e0751572cb507f57172441a6074404f43b51e
asjV2 <ahmad.subaktagin16@gmail.com>
2022-05-18 21:31:40 +0600

    Initial Server Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g2tmf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const tasksName = client.db("toDo").collection("tasks");
+
+    // GET All Tasks
+    app.get("/tasks", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = tasksName.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // POST A Task
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      const result = await tasksName.insertOne(newTask);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Server");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to Port ${port}`);
+});
