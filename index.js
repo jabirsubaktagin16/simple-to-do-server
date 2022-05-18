4a82d43edbdf234afdf1f67a9678d731eecad72d
asjV2 <ahmad.subaktagin16@gmail.com>
2022-05-18 22:04:13 +0600

    Delete Implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -34,6 +34,14 @@ const run = async () => {
     app.post("/tasks", async (req, res) => {
       const newTask = req.body;
       const result = await tasksName.insertOne(newTask);
+      res.send(result);
+    });
+
+    // Delete A Task
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await tasksName.deleteOne(query);
       res.send(result);
     });
   } finally {
